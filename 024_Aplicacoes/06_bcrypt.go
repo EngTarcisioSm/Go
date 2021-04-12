76e42d9c5c045c5cd0b27657f8cf04166052e437
TarcisioSM <tarcsm@gmail.com>
2021-04-12 14:02:10 -0300

    att exercicios

--- 024_Aplicacoes/06_bcrypt.go
+++ 024_Aplicacoes/06_bcrypt.go
@@ -1,26 +1,27 @@
 /*
  	- quando um package tem um "x" quer dizer que o mesmo esta em fase experimental
-	 instalar o package com o comando 
+	 instalar o package com o comando
  	go get -u golang.org/x/crypto/bcrypt
 
-	NÃO FUNCIONOU AINDA 
+	NÃO FUNCIONOU AINDA
 */
 
 package main
 
-import "golang.org/x/crypto/bcrypt"
-import "fmt"
+import (
+	"fmt"
 
+	"golang.org/x/crypto/bcrypt"
+)
 
- func main(){
+func main() {
 	senha := "15011989"
-	
 
-	slicebyte, err := bcrypt.GenerateFromPassword([]byte(senha),10)
+	slicebyte, err := bcrypt.GenerateFromPassword([]byte(senha), 10)
 
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println(slicebyte)
+		fmt.Printf("%s\n", slicebyte)
 	}
- }                                                  
+}
